79b1809027fc968dc261aaba312b779395441845
rajach07 <135721001+rajach07@users.noreply.github.com>
2023-06-29 11:25:05 -0400

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/raja', (req, res) => {
-  res.send('hie this raja .how r u!')
+app.get('/praneeth', (req, res) => {
+  res.send('hie this praneeth .how r u!')
 })
 
 app.listen(port, () => {
